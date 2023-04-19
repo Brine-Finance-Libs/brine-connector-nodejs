c3564ad681b5db4ee6309718ece0257c7a9700a6
Austin <austin.jerry@brine.fi>
2023-04-19 15:41:06 +0400

    refactor: exports signMsgHash

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import { Client } from './client'
 import { WsClient } from './wsClient'
 import { isAuthenticationError } from './error'
 import { signMsg } from './bin/blockchain_utils'
+import { signMsgHash } from './utils'
 
-export { Client, WsClient, isAuthenticationError, signMsg }
+export { Client, WsClient, isAuthenticationError, signMsg, signMsgHash }
 export * from './types'
 
 export default {
   Client,
   WsClient,
   isAuthenticationError,
+  signMsgHash,
 }
